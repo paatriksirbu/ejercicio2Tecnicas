1d7e4c01f61119355c1b480877a89d9e333cfab2
Patrik Paul Sirbu <psirb@myuax.com>
2024-02-11 16:43:08 +0100

    Nuevo metodo para obtener el consumo total del vehiculo durante el trayecto. dist/100  * mediaConsumo nos da el consumo de combustible

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -5,13 +5,15 @@ public class Ejercicio4 {
     public static double calcularVm(double distancia, double tiempo){
         return distancia/tiempo;
     }
+    public static double calcularConsumo(double distancia, double mediaConsumo){
+        return (distancia/100) * mediaConsumo;
+    }
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-
         System.out.println("Algoritmo para calcular la velocidad media." +
-                "\nRecuerde, si quiere poner un numero que no sea entero debe usar coma ',' ");
+                "\nRecuerde, si quiere poner un numero que no sea entero debe usar coma ',' \n");
 
         System.out.println("Introduzca la distancia recorrida en km: ");
         double distancia = sc.nextDouble();
@@ -22,13 +24,18 @@ public class Ejercicio4 {
         System.out.println("Introduzca el tiempo que ha parado en horas (en el caso de haber parado): ");
         double paradas = sc.nextDouble();
 
+        System.out.println("Introduzca la media de consumo del vehiculo: ");
+        double mediaConsumo = sc.nextDouble();
+
         if (paradas > 0 ){
             tiempo += paradas;
         }
 
         double velocidadMedia = calcularVm(distancia, tiempo);
+        double consumoComb = calcularConsumo(distancia, mediaConsumo);
 
-        System.out.println("La velocidad media es de " + velocidadMedia + "km/h");
+        System.out.printf("La velocidad media es de  %.2f km/h \n", velocidadMedia);
+        System.out.printf("El consumo total del trayecto es:  %.2f litros", consumoComb);
     }
 
 }
