b30a69548d2fef14055eea4932c12420744859f6
Patrik Paul Sirbu <psirb@myuax.com>
2024-02-11 16:27:12 +0100

    Muestra lo que se va a hacer y un pequeno recordatorio. Implementacion del tiempo de paradas con un if que se suma al tiempo empleado en realizar el trayecto.

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -9,16 +9,26 @@ public class Ejercicio4 {
 
         Scanner sc = new Scanner(System.in);
 
+
+        System.out.println("Algoritmo para calcular la velocidad media." +
+                "\nRecuerde, si quiere poner un numero que no sea entero debe usar coma ',' ");
+
         System.out.println("Introduzca la distancia recorrida en km: ");
         double distancia = sc.nextDouble();
 
         System.out.println("Introduzca el tiempo en horas empleado en recorrer la distancia: ");
         double tiempo = sc.nextDouble();
 
+        System.out.println("Introduzca el tiempo que ha parado en horas (en el caso de haber parado): ");
+        double paradas = sc.nextDouble();
+
+        if (paradas > 0 ){
+            tiempo += paradas;
+        }
+
         double velocidadMedia = calcularVm(distancia, tiempo);
 
         System.out.println("La velocidad media es de " + velocidadMedia + "km/h");
-
     }
 
 }
